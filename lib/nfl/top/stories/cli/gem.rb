2d8a848604fd82f1a45e40921be8bbfffb006110
Charlie Oster <oster.charlie@yahoo.com>
2020-01-21 23:43:52 +0000

    first commit

---
+++ lib/nfl/top/stories/cli/gem.rb
@@ -0,0 +1,14 @@
+require "nfl/top/stories/cli/gem/version"
+
+module Nfl
+  module Top
+    module Stories
+      module Cli
+        module Gem
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ lib/nfl/top/stories/cli/gem/version.rb
@@ -0,0 +1,11 @@
+module Nfl
+  module Top
+    module Stories
+      module Cli
+        module Gem
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
